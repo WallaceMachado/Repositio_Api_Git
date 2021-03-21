0aa3935fda169c011366f5691fcc8ad4ddab3388
wallacemachado <wallacefm18@gmail.com>
2021-03-21 19:13:02 -0300

    iniciando busca na api GitHub

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';// useState = Hooks
 
 import { FaGithub, FaPlus } from 'react-icons/fa'; // necessário instalar extensão react-icons
 import {Container, Form, SubmitButton} from './styles';
 
+import api from '../../services/api';
+
 export default function Main(){
+  const [newRepo,setNewRepo] = useState('');
+  const [repositorios,setRepositorios] = useState('');
+  
+
+  //useCallback para que quando newRepo ou repositorios for atualizado a função será chamada
+  const handleSubmit = useCallback((e)=>{
+    e.preventDefault();
+
+    async function submit(){ 
+
+      //para teste: facebook/react
+      const response = await api.get(`repos/${newRepo}`);
+  
+      const data = {
+        name: response.data.full_name,
+      }
+  
+      setRepositorios([...repositorios, data]);
+      setNewRepo('');
+    }
+
+    submit();
+
+  }, [newRepo, repositorios]);
+
+  function handleInputChange(e){
+    
+    setNewRepo(e.target.value);
+  }
+
   return(
     <Container>
       
@@ -12,8 +44,12 @@ export default function Main(){
       Meus Repositorios
     </h1>
 
-    <Form onSubmit={()=>{}}>
-      <input type="text" placeholder="Adicionar Repositorios"/>
+    <Form onSubmit={handleSubmit}>
+      <input type="text" 
+      placeholder="Adicionar Repositorios"
+      value={newRepo}
+      onChange={handleInputChange}
+      />
 
       <SubmitButton>
         <FaPlus color="#FFF" size={14}/>
