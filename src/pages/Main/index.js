b5a1ce2533f4d9b7a6fd37fc9eba10f1a9715309
wallacemachado <wallacefm18@gmail.com>
2021-03-23 08:42:35 -0300

    Listando repositórios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,13 @@
 import React, {useState, useCallback} from 'react';// useState = Hooks
 
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa'; // necessário instalar extensão react-icons
-import {Container, Form, SubmitButton} from './styles';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa'; // necessário instalar extensão react-icons
+import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
 
 import api from '../../services/api';
 
 export default function Main(){
   const [newRepo,setNewRepo] = useState('');
-  const [repositorios,setRepositorios] = useState('');
+  const [repositorios,setRepositorios] = useState([]);
   const [loading, setLoading] = useState(false); // para ter loding na tela quando uma requisição tiver sendo feita
 
   //useCallback para que quando newRepo ou repositorios for atualizado a função será chamada
@@ -44,6 +44,12 @@ export default function Main(){
     setNewRepo(e.target.value);
   }
 
+  
+  const handleDelete = useCallback((repo)=> {
+    const find = repositorios.filter(r => r.name !== repo);
+    setRepositorios(find);
+  }, [repositorios]);
+
   return(
     <Container>
       
@@ -68,6 +74,23 @@ export default function Main(){
       </SubmitButton>
 
     </Form>
+
+    <List>
+         {repositorios.map(repo => (
+           <li key={repo.name}>
+             <span>
+             <DeleteButton onClick={()=> handleDelete(repo.name) }>
+                <FaTrash size={14}/>
+             </DeleteButton>  
+             {repo.name}
+             </span>
+             <a href="">
+               <FaBars size={20}/>
+             </a>
+           </li>
+         ))} 
+      </List>
+
 
   </Container>
   )
